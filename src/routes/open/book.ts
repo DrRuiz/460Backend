6b5e2038bb2a8b9778a9dd4403e811486b775e28
DRuiz <93886686+DrRuiz@users.noreply.github.com>
2024-04-29 19:10:16 -0700

    Added comments to book file
    Class 4/29/2024

--- src/routes/open/book.ts
+++ src/routes/open/book.ts
@@ -12,14 +12,26 @@ const format = (resultRow) =>
 
 /**
  * @api {get} /book Request to get all information on all books
- *
- * @apiDescription Request to get all entries of books
- *
+ * @apiDescription Request to get all entries of all books
  * @apiName getAllBooks
  * @apiGroup Book
  *
  * @apiSuccess {String[]} entries Book entries with the following format:
- *              "isbn: {<code>isbn13</code>}  - author: [<code>author</code>] - publish year: [<code>publication_year</code>] - original title: [<code>original_title</code>] - title: [<code>title</code>]..."
+ * "isbn: {<code>isbn</code>}
+ * author: [<code>author</code>]
+ * publish year: [<code>publication</code>]
+ * original title: [<code>original_title</code>]
+ * title: [<code>title</code>]
+ * average rating: [<code>average</code>]
+ * total ratings: [<code>count</code>]
+ * 1 star ratings: [<code>rating_1</code>]
+ * 2 star ratings: [<code>rating_2</code>]
+ * 3 star ratings: [<code>rating_3</code>]
+ * 4 star ratings: [<code>rating_4</code>]
+ * 5 star ratings: [<code>rating_5</code>]
+ * cover (large): [<code>large</code>]
+ * cover (small): [<code>small</code>]"
+ * 
  */
 bookRouter.get('/getAll', (request: Request, response: Response) => {
     const theQuery = 'SELECT * FROM books';
@@ -37,5 +49,81 @@ bookRouter.get('/getAll', (request: Request, response: Response) => {
             });
         });
 });
+
+/**
+ * @api {get} /book/?author=(authorName) Request to get all information related to the books with the provided author name
+ * @apiDescription Request to get all entries of the books with the provided <code>author<code>
+ * @apiName getAllBooksByAuthor
+ * @apiGroup Book
+ *
+ * @apiQuery {string} auhor The name of the author whose books to retrieve
+ *
+ * @apiSuccess {String[]} entries Book entries with the following format:
+ * "isbn: {<code>isbn</code>}
+ * author: [<code>author</code>]
+ * publish year: [<code>publication</code>]
+ * original title: [<code>original_title</code>]
+ * title: [<code>title</code>]
+ * average rating: [<code>average</code>]
+ * total ratings: [<code>count</code>]
+ * 1 star ratings: [<code>rating_1</code>]
+ * 2 star ratings: [<code>rating_2</code>]
+ * 3 star ratings: [<code>rating_3</code>]
+ * 4 star ratings: [<code>rating_4</code>]
+ * 5 star ratings: [<code>rating_5</code>]
+ * cover (large): [<code>large</code>]
+ * cover (small): [<code>small</code>]"
+ * 
+ * @apiError (404: No messages) {String} message "No books with <code>author</code> found"
+ */
+// bookRouter.get(, () => {
+
+// });
+
+/**
+ * @api {get} /book/?rating=(bookRating) Request to get all information related to the books that have the provided rating
+ *
+ * @apiDescription Request to get all entries of books with the provided <code>rating<code>
+ *
+ * @apiName getAllBooksWithTheRating
+ * @apiGroup Book
+ *
+ * @apiQuery {int} rating The rating [0-5] of the books to retrieve
+ *
+ * @apiSuccess {String[]} entries Book entries with the following format:
+ * "isbn: {<code>isbn</code>}
+ * author: [<code>author</code>]
+ * publish year: [<code>publication</code>]
+ * original title: [<code>original_title</code>]
+ * title: [<code>title</code>]
+ * average rating: [<code>average</code>]
+ * total ratings: [<code>count</code>]
+ * 1 star ratings: [<code>rating_1</code>]
+ * 2 star ratings: [<code>rating_2</code>]
+ * 3 star ratings: [<code>rating_3</code>]
+ * 4 star ratings: [<code>rating_4</code>]
+ * 5 star ratings: [<code>rating_5</code>]
+ * cover (large): [<code>large</code>]
+ * cover (small): [<code>small</code>]"
+ * 
+ */
+
+/**
+ * @api {get} /book Request for an author's book
+ * 
+ * @apiDescription Request for the specified book title by the specified author
+ * 
+ * @apiQuery {String} author The author's name
+ * @apiQuery {String} title The title of the book
+ * 
+ * @apiSuccess (Success 200) {String[]} entry Specified entry with the form: (see above format)
+ * 
+ * @apiError (400: Author does not exist) {String} message "Author does not exist."
+ * @apiError (400: Book does not exist) {String} message "Book title does not exist."
+ * @apiError (400: Book and Author do not match) {String} message "Book not written by specified author.
+ */
+// bookRouter.get(, () => {
+
+// })
 
 export { bookRouter };
